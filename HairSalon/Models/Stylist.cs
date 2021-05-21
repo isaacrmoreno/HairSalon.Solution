b0cb2116c40aa7e8e051da4e25823b40984f08a2
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-21 11:42:52 -0700

    :sparkles: add stylist model

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace ClairesHairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      Clients = new HashSet<Client>();
+    }
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string HomeTown { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
